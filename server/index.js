9b61a63be2b68ffa9f357f6f5fdf320ebd09f429
Sahil <Sahil2003@gmail.com>
2024-01-28 16:56:02 +0530

    Feat : Added Socket signaling code server side

--- server/index.js
+++ server/index.js
@@ -6,10 +6,22 @@ const { Server, Socket } = require('socket.io');
 const io = new Server();
 const app = express();
 
+const emailToSocketMapping = new Map();
+
 // this is for catching the new socket server / connection 
 // and then passing it to the io server
 io.on('connnection', socket => {
     // signaling code
+    socket.on('join-room', data => {
+        const { roomId, emailId } = data;
+
+        console.log('User', emailId, 'Joined Room', roomId);
+
+        emailToSocketMapping.set(emailId, socket.id); // store the socket id against the email id
+        socket.join(roomId); // join the room
+
+        socket.broadcast.to(roomId).emit('user-joined', emailId); // broadcast to the room that a new user has joined
+    })
 })
 
 app.listen(8000, () => console.log(`HTTP server listening on port 8000`));
