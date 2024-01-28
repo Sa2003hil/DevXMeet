020cdaedc9352419c7b50a7ea861bf303c6b5684
Sahil <Sahil2003@gmail.com>
2024-01-28 19:23:36 +0530

    Feat : Added some Ui and functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import { SocketProvider } from './providers/Socket';
+
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* <Route path="/about" element={<About />} /> */}
-      </Routes>
+      <SocketProvider>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/room/:rooId" element={<h1>Hey this is the room</h1>} />
+        </Routes>
+
+      </SocketProvider>
 
     </div>
   );
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Button } from "@material-tailwind/react";
+import { useSocket } from '../providers/Socket'
+import { useNavigate } from 'react-router-dom';
+
 
 const Home = () => {
+    const { socket } = useSocket();
+    const navigate = useNavigate();
+
+
+    const [email, setemail] = useState();
+    const [roomId, setroomId] = useState();
+
+
+    const handleRoomJoined = ({ roomId }) => {
+        console.log('Room Joined', roomId);
+        navigate(`/room/${roomId}`);
+    }
+
+    useEffect(() => {
+        socket.on('joined-room', handleRoomJoined);
+    }, [socket]);
+
+    const handleRoomJoin = () => {
+        socket.emit('join-room', { emailId: email, roomId });
+    }
+
+
     return (
         <div className='flex items-center m-auto justify-center mt-10'>
 
@@ -19,23 +44,23 @@ const Home = () => {
                         <input
                             class="w-full h-full px-3 py-3 font-sans text-sm font-normal transition-all bg-transparent border rounded-md peer border-blue-gray-200 border-t-transparent text-blue-gray-700 outline outline-0 placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                             placeholder=" "
-                            id="email"
-                            type="text"
-
+                            type="email"
+                            value={email}
+                            onChange={e => setemail(e.target.value)}
                         />
 
                         <label
                             class="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-gray-900 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-gray-900 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
-                            Email
+                            email
                         </label>
                     </div>
                     <div class="relative h-11 w-full min-w-[200px]">
                         <input
                             class="w-full h-full px-3 py-3 font-sans text-sm font-normal transition-all bg-transparent border rounded-md peer border-blue-gray-200 border-t-transparent text-blue-gray-700 outline outline-0 placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                             placeholder=" "
-                            id="password"
-                            type="password"
-
+                            type="text"
+                            value={roomId}
+                            onChange={e => setroomId(e.target.value)}
                         />
                         <label
                             class="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-gray-900 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-gray-900 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
@@ -66,7 +91,7 @@ const Home = () => {
                 </div>
                 <div class="p-6 pt-0">
                     <button
-                        // onClick={onLogin}
+                        onClick={handleRoomJoin}
                         class="block w-full select-none rounded-lg bg-gradient-to-tr from-gray-900 to-gray-800 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                         type="button">
                         Enter Room
